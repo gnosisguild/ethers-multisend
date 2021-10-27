03d2ca6fc9502b16bc60da89586b93e16a6f8947
Jan-Felix <jan-felix.schwarz@gnosis.io>
2021-10-27 11:03:00 +0200

    also check to field in validation

--- src/isValid.ts
+++ src/isValid.ts
@@ -1,8 +1,11 @@
+import { getAddress } from '@ethersproject/address'
+
 import { encodeSingle } from './encodeSingle'
 import { TransactionInput } from './types'
 
 export const isValid = (transactionInput: TransactionInput): boolean => {
   try {
+    getAddress(transactionInput.to)
     encodeSingle(transactionInput)
     return true
   } catch (e) {
---
+++ test/isValid.spec.ts
@@ -0,0 +1,36 @@
+import { expect } from 'chai'
+
+import { createTransaction, isValid, TransactionType } from '../src'
+
+describe('isValid', () => {
+  const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+  it('should return true only if the encoding works', () => {
+    const tx = {
+      ...createTransaction(TransactionType.transferFunds),
+      to: ZERO_ADDRESS,
+    }
+    expect(isValid(tx)).to.equal(false)
+    expect(isValid({ ...tx, amount: '1' })).to.equal(true)
+  })
+
+  it('should return true only if the `to` field is a valid address', () => {
+    const txWithEmptyTo = {
+      ...createTransaction(TransactionType.raw),
+      data: '0x0',
+    }
+    expect(isValid(txWithEmptyTo)).to.equal(false)
+    expect(
+      isValid({
+        ...txWithEmptyTo,
+        to: '0x123',
+      })
+    ).to.equal(false)
+    expect(
+      isValid({
+        ...txWithEmptyTo,
+        to: ZERO_ADDRESS,
+      })
+    ).to.equal(true)
+  })
+})
