436a3f76979eaa58544d036166d63e7806fc04ff
Jan-Felix <524089+jfschwarz@users.noreply.github.com>
2024-05-31 10:22:59 +0200

    by default use MultiSendCallOnly v1.4.1 (#7)
    * by default use MultiSendCallOnly v1.4.1, MultiSend v1.4.1 if the batch includes delegate calls
    
    * fix some outdated links
    
    ---------
    
    Co-authored-by: Jan-Felix <jfschwarz@users.noreply.github.com>

--- src/encodeMulti.ts
+++ src/encodeMulti.ts
@@ -5,7 +5,9 @@ import { pack } from '@ethersproject/solidity'
 import { MetaTransaction, OperationType } from './types'
 
 export const MULTI_SEND_ABI = ['function multiSend(bytes memory transactions)']
-const MULTI_SEND_CONTRACT_ADDRESS = '0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761'
+
+const MULTISEND_141 = '0x38869bf66a61cF6bDB996A6aE40D5853Fd43B526'
+const MULTISEND_CALLONLY_141 = '0x9641d764fc13c8B624c04430C7356C1C7C8102e2'
 
 /// Encodes the transaction as packed bytes of:
 /// - `operation` as a `uint8` with `0` for a `call` or `1` for a `delegatecall` (=> 1 byte),
@@ -29,10 +31,13 @@ const remove0x = (hexString: string) => hexString.substr(2)
 
 // Encodes a batch of module transactions into a single multiSend module transaction.
 // A module transaction is an object with fields corresponding to a Gnosis Safe's (i.e., Zodiac IAvatar's) `execTransactionFromModule` method parameters.
-// For more information refer to https://docs.gnosis.io/safe/docs/contracts_details/#gnosis-safe-transactions.
 export const encodeMulti = (
   transactions: readonly MetaTransaction[],
-  multiSendContractAddress: string = MULTI_SEND_CONTRACT_ADDRESS
+  multiSendContractAddress: string = transactions.some(
+    (t) => t.operation === OperationType.DelegateCall
+  )
+    ? MULTISEND_141
+    : MULTISEND_CALLONLY_141
 ): MetaTransaction => {
   const transactionsEncoded =
     '0x' + transactions.map(encodePacked).map(remove0x).join('')
@@ -44,7 +49,7 @@ export const encodeMulti = (
 
   return {
     operation: OperationType.DelegateCall,
-    to: multiSendContractAddress || MULTI_SEND_CONTRACT_ADDRESS,
+    to: multiSendContractAddress,
     value: '0x00',
     data,
   }
